9e69091de9debe2aefcc03e99ab13d1723424822
Alessio Michelini <amichelini@hubspot.com>
2021-12-06 10:46:47 +0000

    Day 3 solution 2

--- day3.js
+++ day3.js
@@ -1,11 +1,11 @@
 import { fileToArray } from './lib/utils.mjs';
 
-const findCommon = (list, position) => {
+const findCommon = (list, position, bitToFind = '1') => {
   let ones = 0;
   for (let i = 0; i < list.length; i++) {
     const binary = list[i];
     const bit = binary.slice(position, position +1);
-    if (bit === '1') {
+    if (bit === bitToFind) {
       ones++;
     }
   }
@@ -38,4 +38,31 @@ const solution1 = () => {
   console.log(`Solution: ${power}`);
 }
 
-solution1();
+const solution2 = () => {
+  const bits = fileToArray('./assetts/day3.txt');
+  const binaryLength = bits[0].length;
+  let oxygenList = [...bits];
+  let co2List = [...bits];
+  for (let pos = 0; pos < binaryLength; pos++) {
+    // Find oxygen
+    const ones = findCommon(oxygenList, pos);
+    const commonBit = (ones >= oxygenList.length / 2) ? '1' : '0';
+    if (oxygenList.length > 1) {
+      oxygenList = oxygenList.filter(item => item[pos] === commonBit);
+    }
+    // Finx CO2
+    const zeroes = findCommon(co2List, pos, '0');
+    const leastCommonBit = (zeroes <= co2List.length / 2) ? '0' : '1';
+    if (co2List.length > 1) {
+      co2List = co2List.filter(item => item[pos] === leastCommonBit);
+    }
+  }
+
+  const oxygen = parseInt(oxygenList[0],2);
+  const co2 = parseInt(co2List[0],2);
+  const lifeSupport = oxygen * co2;
+  console.log(`Life Support value: ${lifeSupport}`);
+}
+
+// solution1();
+solution2();
